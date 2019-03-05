b24d7535029bdd4c6859138da5970286da0e7def
ethanjpark <park.ethan@u.northwestern.edu>
2019-03-04 18:39:20 -0600

    changed launch file to not include waypoint file, debugged plume tracing so that the go to service call works! IT'S ALIVEEEEE

--- src/cptbbp.py
+++ src/cptbbp.py
@@ -10,11 +10,12 @@
 
 import rospy
 import numpy as np
-import uuv_waypoints
 
+from std_msgs.msg import Header
 from uuv_control_msgs.srv import GoTo
+from uuv_control_msgs.msg import Waypoint
 from uuv_sensor_ros_plugins_msgs.msg import ChemicalParticleConcentration
-from geometry_msgs.msg import Vector3, Twist, TwistWithCovariance
+from geometry_msgs.msg import Point, Vector3, Twist, TwistWithCovariance
 from nav_msgs.msg import Odometry
 
 THRESHOLD = 0.1 #particle concentration threshold for detecting plume
@@ -23,20 +24,20 @@ BETA_OFFSET = 20 #angle offset relative to upflow
 UPFLOW = np.array([-1.0, 0.0]) #180 rotation of CURRENT_FLOW
 LAMBDA = 500000000 #plume detection time threshold (0.5 seconds)
 
-alg_state = -1			#global var for which state the algorithm is currently in
-particle_concentration 	#global var for particle concentration
-auv_location 			#global var for robot position
-auv_heading 			#global var for robot heading vector
-lhs 					#global var for which side of plume robot will drive out of
-t_last					#global var for last time at which plume was detected
-lost_pnts = []			#last detection points stored when track out is triggered
-ldp						#global var for last detection point
+alg_state = -1                  #global var for which state the algorithm is currently in
+particle_concentration = 0.0    #global var for particle concentration
+auv_location = None             #global var for robot position
+auv_heading = None              #global var for robot heading vector
+lhs = 0                     	#global var for which side of plume robot will drive out of
+t_last = 0                  	#global var for last time at which plume was detected
+lost_pnts = []          		#last detection points stored when track out is triggered
+ldp = None                     	#global var for last detection point
 
 #Calculate angle between two vectors (counter-clockwise positive)
 def angle_between(v1,v2):
 	rad1 = np.arctan2(v1[1],v1[0]) #arctan2 args are y,x (weird)
-    rad2 = np.arctan2(v2[1],v2[0])
-    return np.rad2deg(rad2-rad1)
+	rad2 = np.arctan2(v2[1],v2[0])
+	return np.rad2deg(rad2-rad1)
 
 #Track In behavior of algorithm
 def track_in(gotoservice):
@@ -60,8 +61,8 @@ def track_in(gotoservice):
 		offsetrad = lhs*np.deg2rad(BETA_OFFSET)
 		rotmatrix = np.array([[np.cos(offsetrad), -np.sin(offsetrad)],[np.sin(offsetrad), np.cos(offsetrad)]])
 		new_heading = np.dot(UPFLOW,rotmatrix) #2D heading
-		3D_heading = np.array([new_heading[0],new_heading[1],0.0])
-		new_waypoint = np.add(3D_heading,auv_location)
+		threed_heading = np.array([new_heading[0],new_heading[1],0.0])
+		new_waypoint = np.add(threed_heading,auv_location)
 
 		#creating the waypoint message
 		wp = uuv_waypoints.Waypoint(
@@ -79,7 +80,8 @@ def track_in(gotoservice):
 			interpolator = rospy.get_param('~interpolator', 'lipb')
 			res = gotoservice(wp,wp.max_forward_speed,String(interpolator))
 			print("Go To service call successful: " + String(res))
-
+		except rospy.ServiceException, e:
+			print("Service call failed: %s"%e)
 	elif(rospy.get_rostime() - t_last > LAMBDA): #go to track-out
 		lost_pnts.append(ldp)
 		alg_state = 3
@@ -110,22 +112,30 @@ if __name__=='__main__':
 		Odometry,
 		readauvpose)
 
-	goto = rospy.ServiceProxy('go_to', GoTo)
+	interpolator = rospy.get_param('~interpolator', 'lipb')
+	print("waiting for go to service!")
+	try:
+		rospy.wait_for_service('rexrov2/go_to', timeout=15)
+	except rospy.ROSException:
+		raise rospy.ROSException('Service not available!')
+	print("creating serviceproxy instance for goto!")
+	try:
+		goto = rospy.ServiceProxy('rexrov2/go_to', GoTo)
+	except rospy.ROSException as e:
+		raise rospy.ROSException('Service call failed, error=%s', str(e))
 
-	while(not rospy.is_shutdown()):
+	iwp = Waypoint()
+	iwp.header.stamp = rospy.Time.now()
+	iwp.header.frame_id = "world"
+	iwp.point.x = 5
+	iwp.point.y = 0
+	iwp.point.z = -24
+	iwp.max_forward_speed = 0.4
+	iwp.heading_offset = 0.0
+	iwp.use_fixed_heading = False
+
+	res = goto(iwp,iwp.max_forward_speed, str(interpolator))
+	print("Initial Go To service call successful: " + str(res))
+
+	# while(not rospy.is_shutdown()):
 		#since finding is not implemented yet, go to fixed point where plume will be
-		
-		iwp = uuv_waypoints.Waypoint(
-			x=5,
-			y=0,
-			z=-23,
-			max_forward_speed=0.4,
-			heading_offset=0.0,
-			use_fixed_heading=False,
-			inertial_frame_id="world")
-		#rosservice call to Go_To
-		rospy.wait_for_service('go_to')
-		try:
-			interpolator = rospy.get_param('~interpolator', 'lipb')
-			res = goto(iwp,iwp.max_forward_speed,String(interpolator))
-			print("Initial Go To service call successful: " + String(res))
